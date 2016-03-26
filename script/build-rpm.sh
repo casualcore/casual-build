d27c072a840014e8eadd87b9691c6924bc8b1c31
Hans Bergkvist <hansb112@yahoo.se>
2016-03-26 21:26:20 +0100

    Removing rpmbuild of dev temporarily

--- script/build-rpm.sh
+++ script/build-rpm.sh
@@ -8,5 +8,5 @@ SPEC_ROOT=$4
 mkdir -p RPM/BUILD RPM/RPMS RPM/SOURCES RPM/SPECS RPM/SRPMS
 
 rpmbuild -bb --define "source_root $SOURCE_ROOT" --define "casual_version $CASUAL_VERSION" --define "casual_release $CASUAL_RELEASE" $SPEC_ROOT/RPM-SPECS/casual-middleware.spec
-rpmbuild -bb --define "source_root $SOURCE_ROOT" --define "casual_version $CASUAL_VERSION" --define "casual_release $CASUAL_RELEASE" $SPEC_ROOT/RPM-SPECS/casual-middleware-dev.spec
+#rpmbuild -bb --define "source_root $SOURCE_ROOT" --define "casual_version $CASUAL_VERSION" --define "casual_release $CASUAL_RELEASE" $SPEC_ROOT/RPM-SPECS/casual-middleware-dev.spec
 
