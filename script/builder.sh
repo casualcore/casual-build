1f29a402aa539d3833d79fc210d825aa82e69b0c
Hans Bergkvist <hansb112@yahoo.se>
2016-03-26 19:03:36 +0100

    Adding install and tar

--- script/builder.sh
+++ script/builder.sh
@@ -2,4 +2,6 @@
 cd /git/casual
 casual-make make && casual-make clean && casual-make compile && casual-make link 
 ISOLATED_UNITTEST_DIRECTIVES="--gtest_output='xml:report.xml'" casual-make --debug -a -i test
+casual-make install
+tar cvf /git/casual/casual.tar /usr/local/casual
 casual-make clean
