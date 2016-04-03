9e2101f9bf36b418023f2196dcb826be8f1388c5
Hans Bergkvist <hans.bergkvist@gmail.com>
2016-04-03 16:32:18 +0200

    Adding some more cleaning

--- script/builder.sh
+++ script/builder.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 cd /git/casual
-rm -rf $WORKSPACE/*
+git clean
 casual-make make && casual-make clean && casual-make compile && casual-make link 
 ISOLATED_UNITTEST_DIRECTIVES="--gtest_output='xml:report.xml'" casual-make --debug -a -i test
 casual-make install
