0b63e0b7fe900d5fdb92dad8e3ecbe4b28349daf
Hans Bergkvist <hans.bergkvist@gmail.com>
2016-04-03 17:13:16 +0200

    Adding some more cleaning

--- script/builder.sh
+++ script/builder.sh
@@ -5,4 +5,8 @@ ISOLATED_UNITTEST_DIRECTIVES="--gtest_output='xml:report.xml'" casual-make --deb
 casual-make install
 tar cvf /git/casual/casual.tar /usr/local/casual
 casual-make clean
+find middleware -name "obj" | xargs rm -rf
+find middleware -name "bin" | xargs rm -rf
+find middleware -name ".unttest_domain_home" | xargs rm -rf
+find middleware -name ".casual" | xargs rm -rf
 
