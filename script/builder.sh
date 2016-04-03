060bb90b4769fe18688838e2719f17bc0497c076
Hans Bergkvist <hans.bergkvist@gmail.com>
2016-04-03 16:43:29 +0200

    Adding some more cleaning

--- script/builder.sh
+++ script/builder.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 cd /git/casual
-git clean
+find . -name "obj" | xargs rm -rf
+find . -name "bin" | xargs rm -rf
 casual-make make && casual-make clean && casual-make compile && casual-make link 
 ISOLATED_UNITTEST_DIRECTIVES="--gtest_output='xml:report.xml'" casual-make --debug -a -i test
 casual-make install
 tar cvf /git/casual/casual.tar /usr/local/casual
 casual-make clean
-rm -rf $WORKSPACE/casual/middleware/.casual $WORKSPACE/casual/middleware/.unittest_domain_home 
 
