7be2655b3da1f711b949534336f66fbfb973011d
Hans Bergkvist <hansb112@yahoo.se>
2016-09-04 18:05:29 +0200

    Fixing loggissue

--- script/builder.sh
+++ script/builder.sh
@@ -6,8 +6,8 @@ ISOLATED_UNITTEST_DIRECTIVES="--gtest_output='xml:report.xml'" casual-make test
 casual-make install
 tar cvf /git/casual/casual.tar /usr/local/casual
 casual-make clean
-find . -name ".casual" | xargs rm -rf
-find middleware -name "obj" | xargs rm -rf
-find middleware -name "bin" | xargs rm -rf
-find middleware -name ".unttest_domain_home" | xargs rm -rf
-chmod +w -R .
+#find . -name ".casual" | xargs rm -rf
+#find middleware -name "obj" | xargs rm -rf
+#find middleware -name "bin" | xargs rm -rf
+#find middleware -name ".unttest_domain_home" | xargs rm -rf
+chmod a+w -R .
