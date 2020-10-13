d1c6970fa748d08b75a5a816e60bdf1005a58667
Mathias Creutz <mathiascreutz@yahoo.se>
2020-10-13 17:02:25 +0200

    wip

--- integration-test/images/casual/casual-base/uid_entrypoint.sh
+++ integration-test/images/casual/casual-base/uid_entrypoint.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 if ! whoami &> /dev/null; then
-    if [ -w /etc/passwd]; then
-	echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
-    fi
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
 fi
+
 exec casual-domain-manager -c $CASUAL_DOMAIN_HOME/configuration/domain.yaml
